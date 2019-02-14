44a4e16360169b79bd08c7c9b789fd21f099f26e
John <john.obrien@gmail.com>
2019-02-13 21:24:48 -0500

    Got a single button to extend and retract the linear actuator.

--- open_door_test.py
+++ open_door_test.py
@@ -1,21 +1,21 @@
 from smc import SMC
 import time
 
-mc = SMC('/dev/ttyUSB0', 115200)
+mc = SMC('/dev/ttyACM0', 115200)
 # open serial port and exit safe mode
 mc.init()
 
 # drive using 12b mode
 mc.speed(1000)
-time.sleep(3)
+time.sleep(30)
 mc.speed(-1000)
-time.sleep(3)
+time.sleep(30)
 
 # drive using 7b mode
 mc.speed7b(100)
-time.sleep(3)
+time.sleep(30)
 mc.speed7b(-100)
-time.sleep(3)
+time.sleep(30)
 
 # and stop motor
 mc.stop()
--- push_button.py
+++ push_button.py
@@ -1,7 +1,32 @@
+import time
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+from smc import SMC
+
+
+def print_counter(n):
+    for i in range(1, n):
+        print(i)
+        time.sleep(1)
+
 
 def button_callback(channel):
     print("Button was pushed!")
+    mc = SMC('/dev/ttyACM0', 115200)
+
+    # drive using 12b mode
+    print("Arm extending...")
+    mc.init()
+    mc.speed(-1000)
+    print_counter(15)
+    print("Arm stopped...")
+    mc.stop()
+    print_counter(10)
+    print("Arm retracting...")
+    mc.init()
+    mc.speed(1000)
+    print_counter(15)
+    mc.stop()
+
 
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
@@ -12,3 +37,5 @@ GPIO.add_event_detect(10,GPIO.RISING,callback=button_callback) # Setup event on 
 message = input("Press enter to quit\n\n") # Run until someone presses enter
 
 GPIO.cleanup() # Clean up
+
+
